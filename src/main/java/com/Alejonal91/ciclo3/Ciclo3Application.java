518a3a37cc776f032450e909f101327f0828e0b4
AALEJO <aalejonal91@gmail.com>
2022-08-24 21:53:09 -0500

    spring boot

--- src/main/java/com/Alejonal91/ciclo3/Ciclo3Application.java
+++ src/main/java/com/Alejonal91/ciclo3/Ciclo3Application.java
@@ -1,13 +1,22 @@
 package com.Alejonal91.ciclo3;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication( exclude ={SecurityAutoConfiguration.class} )
 public class Ciclo3Application {
+	@GetMapping ("/hello")
+	public String hello(){
+		return "Ciclo 3..Ojala esto se pueda!!!";
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ciclo3Application.class, args);
+
+		SpringApplication.run
+				(Ciclo3Application.class, args);
 	}
 
 }
